1c7ac08a6f9fad3f95da915e1e30a9b483897aa7
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-08-19 12:05:58 +0530

    Add files via upload

---
+++ App2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class App2 {
+	 public static void main(String[] args) 
+	    {
+
+		int a=10;
+		int b=15;
+		
+		if(a>b) {
+            System.out.println("a is gater");
+		}
+		else {
+			System.out.println("b is gater");
+		}
+	    }
+
+}
