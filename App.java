ae7b7e91bb271fa639aa453d951eae53239b3f7f
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-08-19 12:17:09 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.java.pack;
+
+//Write a Java program that takes a number in inches, converts it to meters.
+//Note: One inch is 0.0254 meter.
+public class App {
+	
+	 public static void main(String[] args) 
+	    {
+
+		int num= 5;
+		String check =(num>0)? "this is positive number": "this is negetive number";
+         System.out.println(check);
+	    }
+	}
+
