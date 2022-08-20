d5242a5dda153a686bc8cd1eddf1b0215c5a3cc0
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-08-20 20:54:05 +0530

    Add files via upload

---
+++ Swap_two_num.java
@@ -0,0 +1,14 @@
+
+public class Swap_two_num {
+	public static void main(String [] args) {
+		int a,b;
+		a=15;
+		b=150;
+		
+		int temp= a;
+		     a=b;
+		     b=a;
+		System.out.println(a);
+	}
+
+}
