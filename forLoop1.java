00ceadcfb2ec384c1c3c6efae457c1bdb9b08f66
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-08-22 11:24:18 +0530

    Add files via upload

---
+++ forLoop1.java
@@ -0,0 +1,13 @@
+package forLoop;
+
+public class forLoop1 {
+//	WAP to print even numbers from 1 to 10.
+	public static void main(String [] args) {
+		for (int i=0; i<=10; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
