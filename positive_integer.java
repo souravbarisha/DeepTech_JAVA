d69bbdeb96b647145518f595925b57ead8d27668
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-08-25 10:31:40 +0530

    Add files via upload

---
+++ positive_integer.java
@@ -0,0 +1,15 @@
+package positive_integer;
+
+import java.util.Scanner;
+
+public class positive_integer {
+public static void main(String[] args) {
+	String num1,num2;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first number");
+	num1=sc.nextLine();
+	System.out.println("Enter first number");
+	num2=sc.nextLine();
+	System.out.println(num1+num2);
+}
+}
