d33c2f529095d01bfb0d9872e7c63305f6165ab6
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-08-30 12:04:52 +0530

    Add files via upload

---
+++ date-30.08.22/super/RactangleMain.java
@@ -0,0 +1,17 @@
+package oops.pack;
+
+import java.util.Scanner;
+
+public class RactangleMain {
+	public static void main(String []args) {
+		
+Rectangle re = new Rectangle(10,15);
+Squares sq = new Squares(10);
+re.area();
+re.perimeter(); 
+
+sq.area();
+sq.perimeter();
+	}
+}
+
