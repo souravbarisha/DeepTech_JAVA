276b5a5724b70442e0d381e5c432df9177e23b29
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-09-12 10:29:27 +0530

    Add files via upload

---
+++ date-09.09.22/BoubleSort/BoubleSort.java
@@ -0,0 +1,21 @@
+package pack.array;
+
+public class BoubleSort {
+public static void main (String [] args) {
+	
+	int [] arr = {4,3,5,2,8,6,7,12,11,13,15};
+	for(int i=0;i<arr.length-1;i++) {
+		for(int j=0;j<arr.length-1-i;j++) {
+			if(arr[j] > arr[j+1]) {
+			int temp = arr[j+1];
+			arr[j+1] = arr[j];
+			arr[j] = temp;
+			
+			}
+		}
+	}
+	for(int i=0; i<arr.length; i++) {
+		System.out.print(arr[i]+" ");
+	}
+}
+}
