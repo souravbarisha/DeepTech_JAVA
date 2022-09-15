8a92f4e0082923abd59c5e6504d0239100e056db
souravbarisha <100298703+souravbarisha@users.noreply.github.com>
2022-09-15 14:41:32 +0530

    Add files via upload

---
+++ date-13.09.22/RemovedAllCharWhichAreINFirst/RemovedAll.java
@@ -0,0 +1,33 @@
+package pack.array;
+//Write a Java Program to Remove all Characters in Second String which are Present in First String.
+public class RemovedAll {
+	public static void main(String args[]) {
+
+		String s1="hello";
+		String s2="world";
+		String newstr="";
+		String n="";
+		char[] cs1=s1.toCharArray();
+		char[] cs2=s2.toCharArray();
+		for(int j=0;j<cs2.length;j++)
+		{
+			for(int i=0;i<cs1.length;i++)
+			{
+				if(cs1[i]==cs2[j]) {
+					cs2[j]=0;
+				}
+			}
+		
+		
+		}
+		
+		for(int i=0;i<cs2.length;i++)
+		{
+			
+			newstr=newstr+String.valueOf(cs2[i]);
+		}
+		System.out.println(newstr);
+		
+		
+	}
+}
