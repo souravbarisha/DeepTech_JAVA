94dd7957cdcf0f746b9029260520d8de1b2a56a9
SOURAV DAS <100298703+souravbarisha@users.noreply.github.com>
2023-02-15 16:10:45 +0530

    Delete JAVA PROJECTS/BMS directory

--- JAVA PROJECTS/BMS/src/main/java/com/BMS/dao/Dao.java
+++
@@ -1,9 +0,0 @@
-package com.BMS.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BMS.entity.Customer;
-
-public interface Dao extends JpaRepository<Customer, Long> {
-
-}
